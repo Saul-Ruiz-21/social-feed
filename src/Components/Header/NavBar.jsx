5cf20a114fa532da433c39bf15e36247b7a860d3
Saul Ruiz <saulruiz730@gmail.com>
2022-10-29 15:23:21 -0500

    added NavBar

---
+++ src/Components/Header/NavBar.jsx
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavB = (props) => {
+    return (
+        <div className='Nav'>
+            <h2>
+                Social Feed
+            </h2>
+        </div>
+    )
+}
+
+
+export default NavB;
---
+++ src/Components/Posts/CreatePosts.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+const CreatePost = (props) => {
+    const [name, setName] = useState('')
+    const {post, setPost} = useState('')
+
+
+}
+
+export default CreatePost
